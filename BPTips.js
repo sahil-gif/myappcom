c237b0d5b7525514f18a851aa5b33cf5ed20731e
sahil-gif <64848036+sahil-gif@users.noreply.github.com>
2020-07-29 21:51:29 +0530

    Add files via upload

---
+++ BPTips.js
@@ -0,0 +1,37 @@
+class BPTips{
+    constructor(){
+        this.show();
+    }
+    display(){
+        this.show();
+    bptipPrev.mousePressed (()=>{
+            this.hide();
+            viewReportNegative.display();
+        })
+    }
+    show(){
+       bptipPrev.show();
+     //  bptip.show();
+       bptip1.show();
+       bptip2.show();
+       bptip3.show();
+       bptip1Advice.show();
+       bptip2Advice.show();
+       bptip3Advice.show();
+       bptipAdvice.show();
+
+
+    }
+    hide(){
+        bptipPrev.hide();
+     //  bptip.hide();
+       bptip1.hide();
+       bptip2.hide();
+       bptip3.hide();
+       bptipAdvice.hide();
+       bptip1Advice.hide();
+       bptip2Advice.hide();
+       bptip3Advice.hide();
+    }      
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# myappcom
+# p5.play-boilerplate
+Boiler plate for p5.play
